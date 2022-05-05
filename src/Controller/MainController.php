849ad484ceeef27bb2fe150ac3d0763f4f9a0c39
coumbelle <emiliemacoumba@gmail.com>
2022-05-05 18:41:55 +0200

    Agence-agent

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,18 +5,31 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\DemandeRepository;
+use App\Repository\InstallationRepository;
+use App\Entity\Demande;
 
 class MainController extends AbstractController
 {
     /**
      * @Route("/", name="main")
      */
-    public function index(): Response
+    public function index(DemandeRepository $demandeRepository, InstallationRepository $installationRepository): Response
     {
         // usually you'll want to make sure the user is authenticated first
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $demandeT=$demandeRepository->findAll();
+        $installationD= $installationRepository->findByUsages('domestique');
+        $demandeD=$demandeRepository->findByInstallation($installationD);
+        $installationN= $installationRepository->findByUsages('non-domestique');
+        $demandeN=$demandeRepository->findByInstallation($installationN);
+
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+            "demandeT" =>$demandeT,
+            "demandeD" =>$demandeD,
+            'demandeN' =>$demandeN,
         ]);
     }
 }
